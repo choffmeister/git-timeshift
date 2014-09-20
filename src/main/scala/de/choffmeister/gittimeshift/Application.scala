c5e319d7cc6e707e9d9e7386ef7b2e755fb16bac
Christian Hoffmeister <hoffmeister@mailbox.org>
2014-09-20 15:40:42 +0200

    Use current dir as default

--- src/main/scala/de/choffmeister/gittimeshift/Application.scala
+++ src/main/scala/de/choffmeister/gittimeshift/Application.scala
@@ -11,11 +11,10 @@ import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 class Application {
   def run(args: Array[String]) {
     val builder = new FileRepositoryBuilder()
-    val repoDir = new File(args(0))
-    val repo = builder.setGitDir(repoDir)
-      .readEnvironment()
-      .findGitDir()
-      .build()
+    val repo = args.headOption match {
+      case Some(d) ⇒ builder.setGitDir(new File(d)).readEnvironment().build()
+      case None ⇒ builder.findGitDir(new File(System.getProperty("user.dir"))).readEnvironment().build()
+    }
 
     val allowed = (t: Date) ⇒ !(t.getDay >= 1 && t.getDay <= 5 && t.getHours >= 8 && t.getHours < 18)
     val timeshiftCleaner = new TimeshiftCommitNodeCleaner(allowed, repo)
